2f69e7b15d394768818bc1e4a785cbf62a782d2b
Jennifer Brabec <jenniferbrabec@gmail.com>
2016-10-22 20:04:18 +0000

    more-practice.sql complete

--- more-practice.sql
+++ more-practice.sql
@@ -3,9 +3,17 @@
 
 
 -- INSERT
-
+INSERT INTO models (year, brand_name, name)
+    VALUES (2015, 'Chevrolet', 'Malibu'),
+           (2015, 'Subaru', 'Outback');
 
 -- CREATE TABLE
-
+CREATE TABLE awards(
+    name VARCHAR(40) NOT NULL,
+    year INTEGER,
+    winner_id INTEGER PRIMARY KEY NOT NULL);
 
 -- More INSERT
+ INSERT INTO awards (name, year, winner_id)
+    VALUES ('IIHS Safety Award', 2015, 49),
+           ('IIHS Safety Award', 2015, 50);
